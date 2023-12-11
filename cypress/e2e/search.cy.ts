d92610031465c0bbc3b0dd01124a5c38318daddf
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-12-11 01:14:02 -0300

    :white_check_mark: add custom commands

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,6 @@
 describe('search products', () => {
   it('should be able to search for a products', () => {
-    cy.visit('/');
-    cy.get('input[name=q]').type('moletom').parent('form').submit();
+    cy.searchByQuery('moletom');
 
     cy.location('pathname').should('include', '/search');
     cy.location('search').should('include', 'q=moletom');
