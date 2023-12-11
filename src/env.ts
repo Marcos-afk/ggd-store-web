eb537211743d757a1568bac24cfdae2407b913bd
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-12-11 02:19:49 -0300

    :wrench: add validations for server/client side environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,21 +1,15 @@
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z
-    .string()
-    .min(1, { message: 'NEXT_PUBLIC_API_URL é requerido' })
-    .url({ message: 'NEXT_PUBLIC_API_URL deve ser uma URL válida' }),
-  APP_URL: z.string().min(1, { message: 'APP_URL é requerido' }).url({
-    message: 'APP_URL deve ser uma URL válida',
-  }),
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 });
-
-const parsedEnv = envSchema.safeParse(process.env);
-
-if (!parsedEnv.success) {
-  throw new Error(
-    `Variáveis de ambiente inválidas: ${parsedEnv.error.flatten().fieldErrors}`,
-  );
-}
-
-export const env = parsedEnv.data;
